b651010fa5ea4102192aa07d3197eea2c32c58eb
Krystian Heberlein <krystianheberlein@gmail.com>
2018-09-12 00:33:20 +0200

    added autotools files

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "hidapi.h"
+#include <stdio.h>
+
+int main() {
+	std::cout << "HELLO AUTOTOOLS WORLD" << '\n';
+
+	 // Enumerate and print the HID devices on the system
+	  struct hid_device_info *devs, *cur_dev;
+
+	  devs = hid_enumerate(0x0, 0x0);
+	  cur_dev = devs;
+	  while (cur_dev) {
+	    printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls",
+	        cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
+	    printf("\n");
+	    printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
+	    printf("  Product:      %ls\n", cur_dev->product_string);
+	    printf("\n");
+	    cur_dev = cur_dev->next;
+	  }
+	  hid_free_enumeration(devs);
+
+	return 0;
+}
